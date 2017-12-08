bf305dfb609b3cb39ab37321a874b50439bf6bc9
Steven Alexander <s.alexander@kainos.com>
2017-12-08 16:25:23 +0000

    leaflet example

---
+++ proof-of-concepts/example-app-leaflet/README.md
@@ -0,0 +1,17 @@
+# Leaflet example app
+
+Proof of concept to test that [Leaflet JS](http://leafletjs.com) can be used for interactive mapping in Street Manager.
+
+Simple Node Express app based on the quick start guide [here](http://leafletjs.com/examples/quick-start/).
+
+## Requires
+
+* NodeJS
+
+## Run
+
+```
+npm install
+npm start
+# runs at http://localhost:3000
+```
---
+++ proof-of-concepts/example-app-leaflet/public/javascripts/application.js
@@ -0,0 +1,34 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoic2FsZXhhbmRlcmthaW5vcyIsImEiOiJjamF4b2d1bHcxMnR5MndwaW9qZDlvMTFkIn0.sYhPbapaMw7viy4rrhR_ZQ'
+}).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
