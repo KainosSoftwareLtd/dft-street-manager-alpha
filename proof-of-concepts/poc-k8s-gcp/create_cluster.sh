a3cad1db745c32d045188462a120032152680007
bbieker <b.bieker@kainos.com>
2018-01-18 09:36:24 +0000

    Updated *_cluster.sh scripts
    Updated *_cluster.sh scripts

--- proof-of-concepts/poc-k8s-gcp/create_cluster.sh
+++ proof-of-concepts/poc-k8s-gcp/create_cluster.sh
@@ -1,29 +1,49 @@
 #!/usr/bin/env bash
 
 # Terraform first
-#cd terraform; terraform init; terraform apply -auto-approve; cd ..
+cd terraform; terraform init; terraform apply -auto-approve; cd ..
 
-# Create GOSSIP cluster with jumphost
+# Assign Cluster Variables
 export NAME=streetworks-alfa-poc.k8s.local
 export PROJECTID=supple-league-190515
-export ZONES=${MASTER_ZONES:-"europe-west2-a,europe-west2-b,europe-west2-c"}
+export PROJECTID=$(gcloud config get-value project)
+export Image_Name="ubuntu-os-cloud/ubuntu-1604-xenial-v20170202"
+export ZONES=${MASTER_ZONES:-"europe-west1-d,europe-west1-c,europe-west1-b"}
+#Not Supported in Alpha
+# export ZONES=${MASTER_ZONES:-"europe-west2-c,europe-west2-a,europe-west2-b"}
+# export ZONES=${MASTER_ZONES:-"europe-west3-b,europe-west3-c,europe-west3-a"}
+# export ZONES=${MASTER_ZONES:-"europe-west4-c,europe-west4-b"}
 export KOPS_STATE_STORE=gs://$(cd terraform && terraform output gce-kubernetes-configuration)
+export NODE_SIZE=${NODE_SIZE:-f1-micro}
+export NODE_COUNT=3
+export MASTER_SIZE=${MASTER_SIZE:-f1-micro}
+export MASTER_COUNT=3
+export TOPOLOGY=private
+export NETWORKING=weave
 export KOPS_FEATURE_FLAGS=AlphaAllowGCE
-kops create cluster ${NAME} \
-  --zones $ZONES \
-  --master-zones $ZONES \
-  --project $PROJECTID \
-  --image "ubuntu-os-cloud/ubuntu-1604-xenial-v20170202" \
-  --node-count 3 \
-  --master-count 3 \
-  --node-size f1-micro \
-  --master-size f1-micro \
-  --topology private \
-  --networking weave \
-  --yes
+echo ${KOPS_STATE_STORE}
+# Create GOSSIP cluster with jumphost
+kops create cluster \
+--name ${NAME} \
+--zones ${ZONES} \
+--project ${PROJECTID} \
+--state ${KOPS_STATE_STORE} \
+--image ${Image_Name} \
+--node-count ${NODE_COUNT} \
+--master-count ${MASTER_COUNT} \
+--node-size ${NODE_SIZE} \
+--master-size ${MASTER_SIZE} \
+--topology ${TOPOLOGY} \
+--networking ${NETWORKING} \
+--yes
+
+#kops get cluster --state ${KOPS_STATE_STORE}/ ${NAME} -oyaml
+
+#  kops delete cluster ${NAME} --yes
 
 until $(kops validate cluster 2>/dev/null >/dev/null); do
   echo "Cluster not ready yet."
   sleep 10
 done
-kops validate cluster
+
+
