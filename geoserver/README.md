0e505851a82c9e32d4e1180e621f2a5e48778257
Michal Schott <schott.michal@gmail.com>
2018-02-13 15:22:40 +0100

    Adding extra variables to geoserver.
    Download config from bucket.

--- geoserver/README.md
+++ geoserver/README.md
@@ -5,7 +5,8 @@ docker build -t local/geoserver .
 
 # Run
 ```
-docker run -p 8080:8080 -v $(pwd)/data:/var/lib/geoserver_data local/geoserver
+docker run -p 8080:8080 -v $(pwd)/data:/var/lib/geoserver_data local/geoserver \
+  -e PG_HOST=... -e PG_PORT=... -e PG_DATABASE=... -e PG_USER=... -e PG_PASSWORD=...
 open http://localhost:8080/geoserver
 ```
 
--- geoserver/docker-entrypoint.sh
+++ geoserver/docker-entrypoint.sh
@@ -2,4 +2,14 @@
 
 env
 
+echo "$GOOGLE_KEY_FILE" > ${HOME}/service-file.json
+
+if [ ! -z ${CONFIG_VERSION} ] && [ ! -z ${CONFIG_BUCKET} ]; then
+  gcloud auth activate-service-account --key-file ${HOME}/service-file.json
+  gcloud config set project $(grep project_id ${HOME}/service-file.json |cut -d\" -f4)
+  gsutil cp gs://${CONFIG_BUCKET}/geoserver-config-${CONFIG_VERSION}.tgz .
+  cd /var/lib/geoserver_data && tar zxf /home/geoserver/geoserver-config-${CONFIG_VERSION}.tgz
+  /var/lib/geoserver_data/install.sh
+fi
+
 exec "$@"
