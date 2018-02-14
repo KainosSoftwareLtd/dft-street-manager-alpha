e08be542c4292f654e308386e5a3322841b717a3
Michal Schott <schott.michal@gmail.com>
2018-02-14 14:52:26 +0100

    Docker improvements.

--- geoserver/docker-entrypoint.sh
+++ geoserver/docker-entrypoint.sh
@@ -9,7 +9,10 @@ if [ ! -z ${CONFIG_VERSION} ] && [ ! -z ${CONFIG_BUCKET} ]; then
   gcloud config set project $(grep project_id ${HOME}/service-file.json |cut -d\" -f4)
   gsutil cp gs://${CONFIG_BUCKET}/geoserver-config-${CONFIG_VERSION}.tgz .
   cd /var/lib/geoserver_data && tar zxf /home/geoserver/geoserver-config-${CONFIG_VERSION}.tgz
-  /var/lib/geoserver_data/install.sh
+fi
+
+if [ -f "/var/lib/geoserver_data/install.sh" ]; then
+  cd /var/lib/geoserver_data && /var/lib/geoserver_data/install.sh
 fi
 
 exec "$@"
